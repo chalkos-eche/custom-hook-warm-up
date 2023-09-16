19b2ce62e72f853f73e89afc05de85ceaf6cdab9
smurf <smurf@kakao.com>
2023-09-17 00:45:11 +0900

    fetchdata

--- src/components/FetchHook.jsx
+++ src/components/FetchHook.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+const baseUrl = "http://jsonplaceholder.typicode.com";
 export const FetchHook = () => {
-  return <div>FetchHook</div>;
+  const [usersData, setUsersData] = useState(null);
+
+  const fetchData = async type => {
+    const response = await fetch(baseUrl + "/" + type);
+    const data = await response.json();
+    console.log(data);
+    setUsersData(data);
+  };
+
+  useEffect(() => {
+    fetchData("users");
+  }, []);
+  return (
+    <>
+      <h1>useFetch!</h1>
+      <button onClick={() => fetchData("users")}>Users</button>
+      <button onClick={() => fetchData("posts")}>Posts</button>
+      <button onClick={() => fetchData("todos")}>Todos</button>
+      <pre>{JSON.stringify(usersData, null, 2)}</pre>
+    </>
+  );
 };
